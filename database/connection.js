7030090864c8d3c373582bc71a873b31a33239a1
Ngocnguyn <ngocnguyen752002@gmail.com>
2023-04-16 00:06:46 +0700

    dotenv

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,12 @@
+const dotenv =  require('dotenv');
+dotenv.config();
 const mysql = require('mysql');
 const connection = mysql.createPool(
     {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'node-sgroup'
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
     }
 )
 
